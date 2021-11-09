b2ded74b8c3475e51e6545408b4dc367bb5133a1
Wilian Pereira <wilian_pereira92@outlook.com>
2021-11-09 11:35:27 -0300

    Adicionando Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+import { About } from '../components';
+
+describe('Testa componente about.js', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(
+      <About />,
+    );
+    const h2El = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(h2El).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    render(
+      <About />,
+    );
+    const primeiroParagrafo = screen
+      .getByText(/This application simulates a Pokédex/);
+    expect(primeiroParagrafo).toBeInTheDocument();
+    const segundoParagrafo = screen
+      .getByText(/One can filter Pokémons by type/);
+    expect(segundoParagrafo).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    render(
+      <About />,
+    );
+    const imgPokedex = screen.getByRole('img');
+    expect(imgPokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
