80b04840e6125af0d6ef9ac396acb35769eb425f
Wilian Pereira <wilian_pereira92@outlook.com>
2021-11-14 18:28:53 -0300

    Adicionado Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+describe('Testa componente FavoritePokemon.js', () => {
+  test('Teste se não tiver pokémon favorito, exibir No favorite pokemon found', () => {
+    render(
+      <FavoritePokemons />,
+    );
+    const noFavorite = screen
+      .getByText(/No favorite pokemon found/);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
